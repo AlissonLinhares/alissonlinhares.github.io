beb98a64f5e8e23e33eeac554ef7e440eae095e6
AlissonLinhares <alisson_linhares@hotmail.com>
2025-04-11 18:53:49 -0300

    Adding more relevante info and fixing image issues

--- assets/images/mini/minify.sh
+++ assets/images/mini/minify.sh
@@ -1,2 +1,4 @@
-mogrify -path . -resize 480x270 -background white -gravity center -extent 480x270 *.jpg
-mogrify -path . -resize 400x300 -background white -gravity center -extent 400x300 *.png
+mogrify -path . -resize x300 *.jpg
+# mogrify -path . -resize 480x270 -background white -gravity center -extent 480x270 *.jpg
+mogrify -path . -resize x300 *.png
+# mogrify -path . -resize 400x300 -background white -gravity center -extent 400x300 *.png
